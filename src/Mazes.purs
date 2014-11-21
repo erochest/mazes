21a3cd80d79520aba45915538f08dcbe026aae73
Eric Rochester <erochest@virginia.edu>
2014-11-21 12:56:27 -0500

    Added dependencies for random numbers and graphs.

--- src/Mazes.purs
+++ src/Mazes.purs
@@ -2,8 +2,12 @@ module Mazes where
 
 
 import Prelude
+import Control.Monad
+import Control.Monad.Eff.Random
+import Data.Graph
 import Debug.Trace
 
 
-main =
+main = do
     print "Hello, world!"
+    random >>= print <<< ((++) "Random!! ") <<< show
